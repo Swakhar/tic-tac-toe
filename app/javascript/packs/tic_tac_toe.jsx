556ccb1409f8e4f16e4e538901f5b8b3fb502317
Swakhar Dey <swakhar@mercury.ai>
2021-06-24 21:59:12 +0200

    Setup react component

--- app/javascript/packs/tic_tac_toe.jsx
+++ app/javascript/packs/tic_tac_toe.jsx
@@ -4,23 +4,12 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
+import Game from '../components/Game'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Game />,
     document.body.appendChild(document.createElement('div')),
   )
 })
